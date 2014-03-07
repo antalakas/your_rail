fa8b78e84e39c491296b5eb43e173e1d16a016a9
Andreas Ntalakas <antalakas@gmail.com>
2014-03-07 16:04:14 +0000

    liveboard setup

--- client/views/map/map.js
+++ client/views/map/map.js
@@ -1,45 +1,102 @@
 var map, markers = [ ];
+var stationsLayer;
+
+/*
+Template.map.helpers({
+
+});
+
+Template.map.ShowStationMarker = function () {
+  var currentStation = Session.get('currentStation');
+  alert(currentStation);
+  // add a marker in the given location, attach some popup content to it and open the popup
+  L.marker([51.5333, 5.7667]).addTo(map)
+  .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
+  .openPopup();
+};
+
+Deps.autorun(function() {
+  var currentStation = Session.get('currentStation');
+  alert(currentStation);
+});
+*/
 
 var initialize = function(element, centroid, zoom, features) {
-    map = L.map(element, {
-        scrollWheelZoom: true,
-        doubleClickZoom: false,
-        boxZoom: false,
-        touchZoom: false
-    }).setView(new L.LatLng(centroid[0], centroid[1]), zoom);
-
-    L.Icon.Default.imagePath = 'packages/leaflet/images'
+  map = L.map(element, {
+    scrollWheelZoom: true,
+    doubleClickZoom: false,
+    boxZoom: false,
+    touchZoom: false
+  }).setView(new L.LatLng(centroid[0], centroid[1]), zoom);
   
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {opacity: .5}).addTo(map);
-    //L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {opacity: .5}).addTo(map);
-    //L.tileLayer.provider('Stamen.Watercolor').addTo(map)
-    
-    map.attributionControl.setPrefix('');
-
-    var attribution = new L.Control.Attribution();
-    attribution.addAttribution("Geocoding data &copy; 2013 <a href='http://open.mapquestapi.com'>MapQuest, Inc.</a>");
-    attribution.addAttribution("Map tiles by <a href='http://stamen.com'>Stamen Design</a> under <a href='http://creativecommons.org/licenses/by/3.0'>CC BY 3.0</a>.");
-    attribution.addAttribution("Data by <a href='http://openstreetmap.org'>OpenStreetMap</a> under <a href='http://creativecommons.org/licenses/by-sa/3.0'>CC BY SA</a>.");
-
-    map.addControl(attribution);
+  L.Icon.Default.imagePath = 'packages/leaflet/images'
+  
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {opacity: .5}).addTo(map);
+  //L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {opacity: .5}).addTo(map);
+  //L.tileLayer.provider('Stamen.Watercolor').addTo(map)
+  
+  map.attributionControl.setPrefix('');
+  
+  var attribution = new L.Control.Attribution();
+  attribution.addAttribution("Geocoding data &copy; 2013 <a href='http://open.mapquestapi.com'>MapQuest, Inc.</a>");
+  attribution.addAttribution("Map tiles by <a href='http://stamen.com'>Stamen Design</a> under <a href='http://creativecommons.org/licenses/by/3.0'>CC BY 3.0</a>.");
+  attribution.addAttribution("Data by <a href='http://openstreetmap.org'>OpenStreetMap</a> under <a href='http://creativecommons.org/licenses/by-sa/3.0'>CC BY SA</a>.");
+  
+  map.addControl(attribution);
 }
 
 Template.map.created = function() {
-
+  
 }
 
 Template.map.rendered = function () {
-    // basic housekeeping
-    $(window).resize(function () {
-        var h = $(window).height(), offsetTop = 90; // Calculate the top offset
-        $('#map_canvas').css('height', (h - offsetTop));
-    }).resize();
-
-    // initialize map events
-    if (!map) {
-        initialize($("#map_canvas")[0], [ 50.5333, 4.7667 ], 8);
+  // basic housekeeping
+  $(window).resize(function () {
+    var h = $(window).height(), offsetTop = 90; // Calculate the top offset
+    $('#map_canvas').css('height', (h - offsetTop));
+  }).resize();
+  
+  // initialize map events
+  if (!map) {
+    initialize($("#map_canvas")[0], [ 50.5333, 4.7667 ], 8);
+  }
+  
+  if (!stationsLayer) {
+    stationsLayer = L.layerGroup();
+  }
+  else {
+    stationsLayer.clearLayers();
+    map.removeLayer(stationsLayer); 
+  }    
+  
+  if (Session.get('currentStationItemContext') == 'stationsList') {
+    var stations = Stations.find().fetch();
+    
+    for (I=0; I<stations.length; ++I) {
+      console.log( stations[I].name );
+      var stationMarker = L.marker([stations[I].latitude, stations[I].longitude]).bindPopup(stations[I].name);
+      stationsLayer.addLayer(stationMarker);
     }
-
+    
+    stationsLayer.addTo(map);
+  }
+  
+  if (Session.get('currentStationItemContext') == 'stationPage') {
+    
+    var station = Session.get('currentStation');
+    console.log( station.name );
+    singleStationMarker = L.marker([station.latitude, station.longitude]).bindPopup(station.name).openPopup();
+    stationsLayer.addLayer(singleStationMarker);
+    
+    stationsLayer.addTo(map);
+  }
+  
+  /*while ( stationsCursor.hasNext() ) {
+        station = stationsCursor.next();
+        console.log( station.name );
+    }*/
+  
+  /*
     // add a marker in the given location, attach some popup content to it and open the popup
     L.marker([50.5333, 4.7667]).addTo(map)
         .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
@@ -50,5 +107,5 @@ Template.map.rendered = function () {
         fillColor: '#f03',
         fillOpacity: 0.5
     }).addTo(map);
-
+    */
 };
--- client/views/stations/station_item.js
+++ client/views/stations/station_item.js
@@ -3,12 +3,56 @@ Template.stationItem.helpers({
     if (Session.get('currentStationItemContext') == 'stationsList') {
       return true;
     }
+    
+    Session.set('currentStation', this);
     return false;
   }
 });
 
 Template.stationItem.created = function() {
-
-    console.log("stationItem.created");
-
+  
+  if (Session.get('currentStationItemContext') == 'stationsList') {
+      return;
+    }
+  
+  console.log("stationItem.created");
+  
+  //https://data.irail.be/NMBS/Liveboard/Simonis/2014/03/07/16/36.json
+  
+  // construct livedata json url
+  var currentdate = new Date(); 
+  var year = currentdate.getFullYear();
+  var month = currentdate.getMonth()+1;
+  var day = currentdate.getDate();
+  var hour = currentdate.getHours();
+  var minutes = currentdate.getMinutes();
+  
+  var liveboardUrl = "https://data.irail.be/NMBS/Liveboard/" + 
+      this.data.name + "/" + 
+      year + "/" + month + "/" + day + "/" + 
+      hour + "/" + minutes + ".json"
+  
+  console.log(liveboardUrl);
+ 
+  Meteor.http.get(liveboardUrl, 
+                  function(error, result){
+                    if(error) {
+                      console.log('http get FAILED!');
+                    } else {
+                      console.log('http get SUCCESS');
+                      if (result.statusCode === 200) {
+                        console.log('Status code = 200!');
+                        console.log('Printing one for testing...');
+                        var liveboard_data = JSON.parse(result.content)
+                        console.log(liveboard_data);
+                        /*console.log(station_data.Stations[0]);
+                        console.log('Adding all stations to Stations collection');
+                        for (I = 0; I < station_data.Stations.length; ++I) {
+                          Stations.insert(station_data.Stations[I]);
+                        }
+                        console.log(station_data.Stations.length.toString() + ' Stations added');*/
+                      }
+                    } 
+                  });
+  
 };
