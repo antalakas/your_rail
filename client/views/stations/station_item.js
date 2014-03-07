5e470f00c655d5c81405fd7e8368a824150446d1
Andreas Ntalakas <antalakas@gmail.com>
2014-03-07 10:01:53 +0000

    preparations for liveboard per station

--- client/views/stations/station_item.js
+++ client/views/stations/station_item.js
@@ -1,3 +1,12 @@
+Template.stationItem.helpers({
+  IsStationList: function () {
+    if (Session.get('currentStationItemContext') == 'stationsList') {
+      return true;
+    }
+    return false;
+  }
+});
+
 Template.stationItem.created = function() {
 
     console.log("stationItem.created");
